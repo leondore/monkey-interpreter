a784db654b338324577b26e600233078905cad40
Leon Dore <leon.dore@gmail.com>
2024-10-05 17:35:38 -0400

    extend repl so it parses commands

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/leondore/monkey-interpreter/lexer"
-	"github.com/leondore/monkey-interpreter/token"
+	"github.com/leondore/monkey-interpreter/parser"
 )
 
 const PROMPT = ">> "
@@ -23,10 +23,23 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		l := lexer.New(line)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
