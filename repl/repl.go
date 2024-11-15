1d24320537139dc70c3b58837f0ddc2d5681aa38
Leon Dore <leon.dore@gmail.com>
2024-11-14 20:01:10 -0400

    implement function evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/leondore/monkey-interpreter/evaluator"
 	"github.com/leondore/monkey-interpreter/lexer"
+	"github.com/leondore/monkey-interpreter/object"
 	"github.com/leondore/monkey-interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -34,7 +36,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
